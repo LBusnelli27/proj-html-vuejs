d1dccca52636676904fbfa3d2b404f1ca2c457f8
Busna27 <lucabusnelli2001@gmail.com>
2022-04-07 15:55:23 +0200

    nav part header

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,8 @@
 </template>
 
 <script>
+import 'bootstrap/dist/css/bootstrap.css';
+
 import Header from './components/Header.vue'
 
 export default {
@@ -16,6 +18,8 @@ export default {
 </script>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700&family=Raleway:wght@100;200;300;400;500;600;700&display=swap');
+
 #app {
   min-width: 1100px;
 }
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,15 +1,80 @@
 <template>
   <div>
-    
+    <section></section>
+
+
+    <section>
+      <nav class="d-flex justify-content-around p-4">
+        <div>
+          <img :src="require('../assets/images/dark-logo.png')" alt="MacCoach Logo">
+        </div>
+        <div>
+          <ul class="d-flex gap-5 list-unstyled">
+            <li v-for="(element, index) in navElements" :key="index">{{ element.name }}</li>
+          </ul>
+        </div>
+        <div class="my-social-container d-flex">
+          <img :src="require('../assets/images/twitter-logo.png')" alt="Twitter Nav Logo">
+          <img :src="require('../assets/images/black-facebook-logo.png')" alt="Facebook Nav Logo">
+          <img :src="require('../assets/images/instagram-logo.png')" alt="Instagram Nav Logo">
+          <img :src="require('../assets/images/linkedin-logo.png')" alt="Twitter Nav Logo">
+        </div>
+      </nav>
+    </section>
   </div>
 </template>
 
 <script>
 export default {
   name : 'HeaderComponent',
+
+  data() {
+    return {
+      navElements : [
+        {
+          name : 'Home',
+          link : '#'
+        },
+        {
+          name : 'Pages',
+          link : '#'
+        },
+        {
+          name : 'Courses',
+          link : '#'
+        },
+        {
+          name : 'Features',
+          link : '#'
+        },
+        {
+          name : 'Blog',
+          link : '#'
+        },
+        {
+          name : 'Shop',
+          link : '#'
+        },
+      ]
+    }
+  },
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+  nav {
+    div{
+      img {
+        width: 150px;
+      }
+    }
+    .my-social-container {
+      gap: 1rem;
+      img {
+        height: 28px;
+        width: 28px;
+        opacity: .5;
+      }
+    }
+  }
 </style>
